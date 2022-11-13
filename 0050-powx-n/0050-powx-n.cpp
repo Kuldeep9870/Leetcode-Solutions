3c605f807e80e3a1b6ba4be52e022786febdfe96
Kuldeep9870 <117843305+Kuldeep9870@users.noreply.github.com>
2022-11-13 12:49:45 +0530

    Time: 0 ms (100.00%), Space: 6 MB (47.51%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
+    double power(double a,int b)
+{
+    if(b==0)
+    return 1;
+
+    if(b==1)
+    return a;
+
+    //recursive call
+    double ans=power(a,b/2);
+
+    if(b%2==0)
+    return ans*ans;
+    else
+    return a*ans*ans;
+    }
+
+
     double myPow(double x, int n) {
-        double ans=x;
         if(n<0)
         {
-            ans=1/ans;
-            n=abs(n);
+            x=1/x;
+            n=labs(n);
         }
-        if(pow(ans,n)>INT_MAX)
+        double ans=power(x,n);
+        if(ans>INT_MAX)
             return 0;
-        
-        ans=pow(ans,n);
-        
-        return ans;
+        else
+            return ans;
         
     }
 };
