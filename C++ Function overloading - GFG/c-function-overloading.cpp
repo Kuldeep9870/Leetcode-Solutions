091b008015d36e69ad01add1373c5f813a928193
Kuldeep9870 <117843305+Kuldeep9870@users.noreply.github.com>
2022-12-14 12:42:40 +0530

    Added solution - LeetHub

---
+++ C++ Function overloading - GFG/c-function-overloading.cpp
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <iostream>
+using namespace std;
+
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+
+void volume(int s)
+{
+    // Add your code here.
+    double vol;
+    vol=s*s*s;
+    cout<<vol<<endl;
+}
+
+void volume(int r, int h)
+{
+    // Add your code here.
+    double vol;
+    vol=3.14159*r*r*h;
+    cout<<vol<<endl;
+}
+
+void volume(int l, int b, int h)
+{
+    // Add your code here.
+    double vol;
+    vol=l*b*h;
+    cout<<vol<<endl;
+}
+
+
+
+//{ Driver Code Starts.
+int main() 
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int q;
+        cin>>q;
+        
+        switch (q)
+        {
+            case 1:
+            int edge;
+            cin>>edge;
+            volume(edge);
+            break;
+            
+            case 2:
+            int radius, heigh;
+            cin>>radius>>heigh;
+            volume(radius, heigh);
+            break;
+            
+            case 3:
+            int length, width, height;
+            cin>>length>>width>>height;
+            volume(length, width, height);
+            break;
+            
+        }
+        
+        
+        
+    }
+	
+	return 0;
+}
+// } Driver Code Ends
