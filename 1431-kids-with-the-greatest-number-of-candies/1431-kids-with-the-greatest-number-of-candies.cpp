4596fabf8d9271664973757da1c0948cc9fb2acb
Kuldeep <117843305+Kuldeep9870@users.noreply.github.com>
2023-04-14 13:49:32 +0530

    Time: 4 ms (61.96%), Space: 8.9 MB (86.50%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int maxi=INT_MIN;
+        for(auto i:candies){
+            maxi=max(i,maxi);
+        }
+        vector<bool> ans;
+        for(auto i:candies){
+            if((i+extraCandies)>=maxi)
+                ans.push_back(true);
+            else
+                ans.push_back(false);
+        }
+        return ans;
+    }
+};
