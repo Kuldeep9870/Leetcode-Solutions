7c95c9718985db71f996b34c83f7d64128ed775f
Kuldeep <117843305+Kuldeep9870@users.noreply.github.com>
2023-05-16 23:23:31 +0530

    Time: 3 ms (60.06%), Space: 6.9 MB (96.02%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
+        
+        //using bit manipulation
         vector<vector<int>> ans;
         int n=nums.size();
         
         for(int i=0;i<(1<<n);i++){
             vector<int> res;
             for(int j=0;j<n;j++){
+                //set bit check of each bit
                 if(i& (1<<j)){
                     res.push_back(nums[j]);
                 }
