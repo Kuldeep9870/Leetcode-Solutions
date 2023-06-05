25afb4b560c88a406f98feef8285154fae61c087
Kuldeep <117843305+Kuldeep9870@users.noreply.github.com>
2023-06-05 23:53:54 +0530

    Added solution - LeetHub

--- Missing number - GFG/missing-number.cpp
+++ Missing number - GFG/missing-number.cpp
@@ -26,6 +26,7 @@ int main()
 int missingNumber(int A[], int N)
 {
     // Your code goes here
+    /*
     //summation method
     
     int sum=N*(N+1)/2;
@@ -34,4 +35,24 @@ int missingNumber(int A[], int N)
         cal=cal+A[i];
         
     return sum-cal;
+    */
+    
+    //XOR approach
+    
+    int xor1=0;
+    int xor2=0;
+    
+    for(int i=0 ;i<N-1;i++){
+        xor1=xor1^A[i];
+        
+        
+        //xor 1 to n-1
+        xor2=xor2^(i+1);
+    }
+    
+    //xor n int xor2
+    xor2=xor2^N;
+    
+    return xor1^xor2;
+    
 }
